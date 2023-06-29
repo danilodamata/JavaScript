7b0ca9fdb0d81ef3c2da53a0e1afe4b78ff744c7
Danilo da Mata <danilusdamata@gmail.com>
2023-06-29 08:28:33 -0300

    ambientes add

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+
+
+
+var c = 1 
+do { 
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=9)
+
+
+
+
+
+
+
+
+
+
+
+
+/* var c = 1
+while ( c<= 6) {
+    console.log('Tudo bem')
+    c ++
+
+}
+*/
