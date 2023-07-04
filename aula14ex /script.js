316bb6df4c0e1ed7a38572375210699f51836c88
Danilo da Mata <danilusdamata@gmail.com>
2023-07-04 09:31:04 -0300

    updates after contando

--- aula14ex /script.js
+++ aula14ex /script.js
@@ -1,17 +1,36 @@
 function contar() {
-  var inicio = parseInt(document.getElementById('ni').value);
-  var fim = parseInt(document.getElementById('nf').value);
-  var passo = parseInt(document.getElementById('pas').value);
-  var i = inicio;
-  document.getElementById("res").innerHTML = i;
-  console.log(i);
-  i += passo;
-  while (i < fim) {
-    document.getElementById("res").innerHTML += ", " + i;
-    console.log(i);
-    i += passo;
+  let inicio = document.getElementById('ni');
+  let fim = document.getElementById('nf');
+  let passo = document.getElementById('pas');
+  let res = document.getElementById('res')
+  
+  if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    window.alert('ERRO! Falta dados!')
+  } else {
+    res.innerHTML = 'Contando:'
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if (p = 0) {
+      window.alert('Passo invalido! Considerando PASSO 1')
+      p =1
+    }
+    if (i < f) {
+      //contagem crescente
+      for(let c = i; c <= f; c += p) {
+        res.innerHTML += `${c} \u{1F449}`
   }
-
-
-
+} else {
+    //contagem regressiva
+      for(let c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1F449}`
+      }
+  }
+  res.innerHTML += `\u{1F3C1}`
 }
+}
+
+
+
+
+
